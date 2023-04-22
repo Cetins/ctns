68df1903ea2efd6730f28654a1460839a42cebc2
Cetins <69481095+Cetins@users.noreply.github.com>
2023-04-22 13:14:57 +0100

    blog icon changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import { FaBlog } from 'react-icons/fa';
+import { AiFillGithub, AiFillLinkedin, AiFillMediumCircle } from 'react-icons/ai';
+import { SiMedium } from 'react-icons/si';
 
 const Footer = () => {
     return (
@@ -9,7 +9,7 @@ const Footer = () => {
           <span className='social'>
             <button className='social-button'><a href='https://github.com/Cetins'><AiFillGithub className='footerIcons'/></a></button>
             <button className='social-button'><a href='https://www.linkedin.com/in/cetins/'><AiFillLinkedin className='footerIcons'/></a></button>
-            <button className='social-button'><a href='https://medium.com/@cetins'><FaBlog className='footerIcons'/></a></button>
+            <button className='social-button'><a href='https://medium.com/@cetins'><SiMedium className='footerIcons'/></a></button>
           </span>
         </footer>
     );

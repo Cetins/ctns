92d05836f4cfa25b3f6babefce209ea6e071f883
Cetins <69481095+Cetins@users.noreply.github.com>
2023-04-25 10:32:55 +0100

    mobile view tweaked on homepage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <h2 className='subtitle'>Junior Software Engineer</h2>
         </header>
         <main>
-            <p className='text'>Hi there! My name is Sezen Cetin, and I'm a software engineer based in Glasgow. One day, I decided to use a drag-and-drop website builder for my small business. I enjoyed it deeply, and one tutorial followed another, until I found myself in CS50x, learning the C language. Since then, programming has been my sole focus and passion</p>
+            <p className='text'>Hi there! My name is Sezen Cetin, and I'm a software engineer based in Glasgow. One day, I decided to use a drag-and-drop website builder for my small business. I enjoyed it deeply, and one tutorial followed another, until I found myself in CS50x, learning the C language. Since then, programming has been my sole focus and passion.</p>
             <Education />
             <Experience />
         </main>
